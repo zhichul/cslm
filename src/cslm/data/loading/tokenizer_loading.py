e270aa98833c0934326f2143c082ff932a95203f
Brian Lu <blu@Brians-MacBook-Pro-19.local>
2021-06-28 21:14:27 -0400

    Add combine_wordlevel_tokenizer

--- src/cslm/data/loading/tokenizer_loading.py
+++ src/cslm/data/loading/tokenizer_loading.py
@@ -1,5 +1,9 @@
 from tokenizers import Tokenizer
-
+from tokenizers.models import WordLevel
+from tokenizers import normalizers
+from tokenizers.normalizers import Lowercase, StripAccents, NFD
+from tokenizers.pre_tokenizers import WhitespaceSplit
+from tokenizers.processors import TemplateProcessing
 
 def setup_tokenizer(tokenizer, max_length=16, pad_token='[PAD]'):
     tokenizer.enable_padding(direction='right', pad_id=tokenizer.token_to_id(pad_token), pad_type_id=0,
@@ -16,3 +20,40 @@ def load_and_setup_tokenizer(file, max_length=16, pad_token='[PAD]'):
     tokenizer = load_tokenizer(file)
     setup_tokenizer(tokenizer, max_length=max_length, pad_token=pad_token)
     return tokenizer
+
+def combine_wordlevel_tokenizer(l1_tokenizer, l2_tokenizer):
+    # get l1 vocab
+    l1_vocab = l1_tokenizer.get_vocab()
+    l1_vocab_size = len(l1_vocab)
+
+    # get l2 vocab and shift
+    l2_vocab = {k: v + l1_vocab_size for k,v in l2_tokenizer.get_vocab().items()}
+    l2_vocab_size = len(l2_vocab)
+
+    # delete redundant items
+    del l2_vocab["[BOS]"]
+    del l2_vocab["[EOS]"]
+    del l2_vocab["[UNK]"]
+    del l2_vocab["[PAD]"]
+
+    # combine vocab
+    combined_vocab = {}
+    for k,v in l1_vocab.items():
+        combined_vocab[k] = v
+    for k,v in l2_vocab.items():
+        combined_vocab[k] = v
+    assert len(combined_vocab) == l1_vocab_size + l2_vocab_size
+
+    # build new tokenizer
+    word_tokenizer = Tokenizer(WordLevel(vocab=combined_vocab, unk_token="[UNK]"))
+    word_tokenizer.normalizer = normalizers.Sequence([NFD(), Lowercase(), StripAccents()])
+    word_tokenizer.pre_tokenizer = WhitespaceSplit()
+    word_tokenizer.post_processor = TemplateProcessing(
+        single="[BOS] $A [EOS]",
+        pair="[BOS] $A [EOS] $B",
+        special_tokens=[
+            ("[BOS]", 1),
+            ("[EOS]", 2),
+        ],
+    )
+    return word_tokenizer
