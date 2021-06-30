249d6b2b8ccaede190813447f986d7bd039599a6
Brian Lu <blu@Brians-MacBook-Pro-19.local>
2021-06-30 11:29:25 -0400

    Add gumbel perturbation feature to beam_search
    1. Stochastic beam search is implemented.
    2. BeamCell has a new merge function.
    3. BeamCell has a compute_weights function that should be called before
       it is frozen.

--- src/cslm/inference/beam_search.py
+++ src/cslm/inference/beam_search.py
@@ -1,4 +1,4 @@
-from cslm.utils import ImmutableDict
+from cslm.utils import ImmutableDict, max_gumbel, log_importance_weight
 from collections import defaultdict
 
 import torch
@@ -7,11 +7,12 @@ import heapq
 
 class BeamCell:
 
-    def __init__(self, size):
+    def __init__(self, size, is_stochastic=False):
         self.size = size
         self.beams = []
         self.worst_score = None
         self.frozen = False
+        self.is_stochastic = is_stochastic
 
     def add(self, score, ids, state):
         if self.frozen:
@@ -32,6 +33,35 @@ class BeamCell:
             beams.append((score, tuple(ids), ImmutableDict({k: v for k, v in state.items()})))
         self.beams = tuple(beams)
 
+    def compute_importance_weights(self):
+        """
+        The values only make sense if it is a stochastic cell
+        :return:
+        """
+        if not self.is_stochastic:
+            raise ValueError("You can only compute weights on beam cells from stochastic beam search.")
+        kappa = self.beams[0][0]
+        self.beams[0][-1]["log_weight"] = -1e9
+        for score, ids, state in self.beams[1:]:
+            state["log_weight"] = log_importance_weight(kappa, state["log_prob"])
+
+    @staticmethod
+    def merge(a, b, size=None):
+        if not a.is_stochastic == b.is_stochastic:
+            raise ValueError("only merge cells that are both stochastic or deterministic")
+        if size is None:
+            size = a.size + b.size
+        is_stochastic = a.is_stochastic and b.is_stochastic
+        o = BeamCell(size=size, is_stochastic=a.is_stochastic)
+        for score, ids, beam in a:
+            o.add(score, ids, beam)
+        for score, ids, beam in b:
+            o.add(score, ids, beam)
+        if is_stochastic:
+            o.compute_importance_weights()
+        o.freeze()
+        return o
+
     def __iter__(self):
         if not self.frozen:
             self.freeze()
@@ -45,7 +75,6 @@ class BeamCell:
     def __len__(self):
         return len(self.beams)
 
-
 def grow_beams(cells=None,
                beam_ids=None,
                tokens=None,
@@ -55,10 +84,12 @@ def grow_beams(cells=None,
                decoder_attention_mask=None,
                state=None,
                cum_log_probs=None,
+               gumbel_max=None,
                extension_state=None,
                extension_bin_assignment=None,
                eos_ids=None,
-               pad_id=None):
+               pad_id=None,
+               do_sample=False):
     """
     Updates the cells in-place, and returns new decoder_input_ids, decoder_attention_mask, cum_log_probs, and state.
 
@@ -75,8 +106,17 @@ def grow_beams(cells=None,
     :param extension_bin_assignment:
     :param eos_ids:
     :param pad_id:
+    :param do_sample:
     :return:
     """
+
+    # perturb if do_sample
+    gumbel_max = gumbel_max.expand_as(cum_log_probs)
+    if do_sample:
+        cum_log_probs_perturbed = torch.where(cum_log_probs == -1e9, cum_log_probs, max_gumbel(cum_log_probs, gumbel_max))
+    else:
+        cum_log_probs_perturbed = cum_log_probs.detach().clone()
+
     # reshaping
     prefixes_view = decoder_input_ids.view(-1, decoder_input_ids.size(-1))
 
@@ -84,28 +124,32 @@ def grow_beams(cells=None,
     next_tokens_by_bin = []
     prefixes_by_bin = []
     cum_log_probs_by_bin = []
+    cum_log_probs_perturbed_by_bin = []
     state_by_bin = defaultdict(list)
     for b in range(num_bins):
         mask = extension_bin_assignment == b
 
         # select bin
         cum_log_probs_of_bin = torch.masked_select(cum_log_probs, mask)
+        cum_log_probs_perturbed_of_bin = torch.masked_select(cum_log_probs_perturbed, mask)
         beam_ids_of_bin = torch.masked_select(beam_ids, mask)
         tokens_of_bin = torch.masked_select(tokens, mask)
         extension_state_of_bin = {k: torch.masked_select(v, mask) for k, v in extension_state.items()}
 
-        # select top k
-        actual_retained_extensions = min(cum_log_probs_of_bin.numel(), (
+        # select top k (w.r.t. perturbed log probs)
+        actual_retained_extensions = min(cum_log_probs_perturbed_of_bin.numel(), (
                 num_bins + 1) * num_beams)  # at most num_bins * num_beams extensions have EOS, and will thus not continue to be searched
-        topk_ret = torch.topk(cum_log_probs_of_bin, actual_retained_extensions, dim=-1)
+        topk_ret = torch.topk(cum_log_probs_perturbed_of_bin, actual_retained_extensions, dim=-1)
         topk_beam_ids_of_bin = beam_ids_of_bin[topk_ret.indices]
         topk_cum_log_probs_of_bin = cum_log_probs_of_bin[topk_ret.indices]
+        topk_cum_log_probs_perturbed_of_bin = cum_log_probs_perturbed_of_bin[topk_ret.indices]
         topk_tokens_of_bin = tokens_of_bin[topk_ret.indices]
         topk_extension_state_of_bin = {k: v[topk_ret.indices] for k, v in extension_state_of_bin.items()}
 
         next_tokens_of_bin = []
         prefixes_of_bin = []
         new_cum_log_probs_of_bin = []
+        new_cum_log_probs_perturbed_of_bin = []
         state_of_bin = defaultdict(list)
         # pull of extensions with EOS and try to fill the bin with the remaining open extensions
         for i in range(actual_retained_extensions):
@@ -115,15 +159,17 @@ def grow_beams(cells=None,
             candidate_next_token = topk_tokens_of_bin[i]
             candidate_beam_id = topk_beam_ids_of_bin[i]
             candidate_cum_log_prob = topk_cum_log_probs_of_bin[i]
+            candidate_cum_log_prob_perturbed = topk_cum_log_probs_perturbed_of_bin[i]
             candidate_prefix = prefixes_view[candidate_beam_id.item()]
             if candidate_next_token.item() in eos_ids:
-                cells[b].add(score=candidate_cum_log_prob.item(),
+                cells[b].add(score=candidate_cum_log_prob_perturbed.item(),
                              ids=candidate_prefix.tolist() + [candidate_next_token.item()],
-                             state={k: v[i].tolist() for k, v in topk_extension_state_of_bin.items()})
+                             state={k: v[i].tolist() for k, v in topk_extension_state_of_bin.items()} | {"log_prob": candidate_cum_log_prob.item()})
             else:
                 next_tokens_of_bin.append(candidate_next_token.tolist())
                 prefixes_of_bin.append(candidate_prefix.tolist())
                 new_cum_log_probs_of_bin.append(candidate_cum_log_prob.tolist())
+                new_cum_log_probs_perturbed_of_bin.append(candidate_cum_log_prob_perturbed.tolist())
                 for k, v in topk_extension_state_of_bin.items():
                     state_of_bin[k].append(v[i].tolist())
         # fill the rest of the bin with padding
@@ -131,17 +177,20 @@ def grow_beams(cells=None,
             j = 0
             candidate_next_token = decoder_input_ids.new_tensor(pad_id)
             candidate_cum_log_prob = cum_log_probs.new_tensor(-1e9)
+            candidate_cum_log_prob_perturbed = cum_log_probs_perturbed.new_tensor(-1e9)
             candidate_prefix = prefixes_view[b * num_beams]
 
             next_tokens_of_bin.append(candidate_next_token.tolist())
             prefixes_of_bin.append(candidate_prefix.tolist())
             new_cum_log_probs_of_bin.append(candidate_cum_log_prob.tolist())
+            new_cum_log_probs_perturbed_of_bin.append(candidate_cum_log_prob_perturbed.tolist())
             for k, v in state.items():
                 state_of_bin[k].append(v[0, b, 0, 0].tolist())
 
         next_tokens_by_bin.append(next_tokens_of_bin)
         prefixes_by_bin.append(prefixes_of_bin)
         cum_log_probs_by_bin.append(new_cum_log_probs_of_bin)
+        cum_log_probs_perturbed_by_bin.append(new_cum_log_probs_perturbed_of_bin)
         for k, v in state_of_bin.items():
             state_by_bin[k].append(v)
     # pack
@@ -151,11 +200,13 @@ def grow_beams(cells=None,
         None, ...]  # batch_size x num_bins x num_beams x new_prefix_length
     decoder_attention_mask = decoder_input_ids.new_ones(decoder_input_ids.size())
     cum_log_probs = torch.tensor(cum_log_probs_by_bin)[None, ..., None]
+    cum_log_probs_perturbed = torch.tensor(cum_log_probs_perturbed_by_bin)[None, ..., None]
     state = {k: torch.tensor(v)[None, ..., None] for k, v in state_by_bin.items()}
     return {
         "decoder_input_ids": decoder_input_ids,
         "decoder_attention_mask": decoder_attention_mask,
         "cum_log_probs": cum_log_probs,
+        "gumbel_max": cum_log_probs_perturbed,
         "state": state
     }
 
@@ -177,6 +228,7 @@ def beam_search(model=None,
                 eos_ids=tuple(),
                 vocab_size=None,
                 pad_id=None,
+                do_sample=False,
                 ):
     # assert beam_size
     batch_size = input_ids.size(0)
@@ -201,7 +253,9 @@ def beam_search(model=None,
     cum_log_probs = torch.full((batch_size, num_bins, num_beams, 1), -1e9, dtype=torch.float32)
     cum_log_probs[0, 0, 0, 0] = 0.0
 
-    cells = [BeamCell(num_return_sequences) for _ in range(num_bins)]
+    gumbel_max = cum_log_probs.new_zeros(cum_log_probs.size())
+
+    cells = [BeamCell(num_return_sequences, is_stochastic=do_sample) for _ in range(num_bins)]
 
     for t in range(max_length - 1):
         decoder_last_layer = model.base_model(
@@ -229,19 +283,27 @@ def beam_search(model=None,
                              decoder_attention_mask=decoder_attention_mask,
                              state=state,
                              cum_log_probs=cum_log_probs,
+                             gumbel_max=gumbel_max,
                              extension_state=extension_state,
                              extension_bin_assignment=extension_bin_assignment,
                              eos_ids=eos_ids,
-                             pad_id=pad_id)
+                             pad_id=pad_id,
+                             do_sample=do_sample)
 
         decoder_input_ids = outputs["decoder_input_ids"]
         decoder_attention_mask = outputs["decoder_attention_mask"]
         cum_log_probs = outputs["cum_log_probs"]
         state = outputs["state"]
+        gumbel_max = outputs["gumbel_max"]
+
+    # fill cell with open beams until full
     for b, cell in enumerate(cells):
         if len(cell) < num_return_sequences:
             for i in range(num_return_sequences - len(cell)):
-                cell.add(cum_log_probs[0, b, i, 0].item(), decoder_input_ids[0, b, i, :].tolist(), {k: v[0, b, i, 0].tolist() for k,v in state.items()})
-    for cell in cells:
+                cell.add(gumbel_max[0, b, i, 0].item(),
+                         decoder_input_ids[0, b, i, :].tolist(),
+                         {k: v[0, b, i, 0].tolist() for k,v in state.items()} | {"log_prob": cum_log_probs[0, b, i, 0].item()})
+        if do_sample:
+            cell.compute_importance_weights()
         cell.freeze()
     return cells
--- src/cslm/utils.py
+++ src/cslm/utils.py
@@ -1,3 +1,4 @@
+from scipy.stats import gumbel_r
 from transformers.utils import logging
 import random
 import numpy as np
@@ -19,6 +20,7 @@ def set_seed(seed: int):
     torch.manual_seed(seed)
     torch.cuda.manual_seed_all(seed)
 
+
 def seq_dot(s1, s2):
     if not isinstance(s1, tuple) or not isinstance(s2, tuple) or len(s1) != len(s2):
         raise ValueError(f"{len(s1)}, {len(s2)}")
@@ -31,6 +33,7 @@ def seq_dot(s1, s2):
         cum += (m1 * m2).sum()
     return cum
 
+
 def seq_norm(s1):
     if not isinstance(s1, tuple):
         raise ValueError()
@@ -42,6 +45,7 @@ def seq_norm(s1):
         cum += (m1 * m1).sum()
     return cum ** 0.5
 
+
 def seq_numel(s1):
     if not isinstance(s1, tuple):
         raise ValueError()
@@ -52,6 +56,7 @@ def seq_numel(s1):
             continue
         cum += m1.numel()
     return cum
+
 
 def seq_add(s1, s2):
     if not isinstance(s1, tuple) or not isinstance(s2, tuple) or len(s1) != len(s2):
@@ -65,6 +70,7 @@ def seq_add(s1, s2):
         results += (m1 + m2,)
     return results
 
+
 def seq_sub(s1, s2):
     if not isinstance(s1, tuple) or not isinstance(s2, tuple) or len(s1) != len(s2):
         raise ValueError(f"{len(s1)}, {len(s2)}")
@@ -77,8 +83,10 @@ def seq_sub(s1, s2):
         results += (m1 - m2,)
     return results
 
+
 def seq_cosine(s1, s2):
     return seq_dot(s1, s2) / (seq_norm(s1) * seq_norm(s2))
+
 
 def seq_requires_grad(s1):
     if not isinstance(s1, tuple):
@@ -96,3 +104,48 @@ class ImmutableDict(dict):
 
     def __set__(self, instance, value):
         raise ValueError("Immutable")
+
+
+def log1mexp(a):
+    ret = torch.where(a > -0.693, torch.log(-expm1(a)), log1p(- torch.exp(a)))
+    return ret
+
+
+def log1pexp(a):
+    ret = torch.where(a < 18, log1p(torch.exp(a)), a + torch.exp(-a))
+    return ret
+
+
+def log1p(a):
+    return torch.log(1 + a)
+
+
+def expm1(a):
+    return torch.exp(a) - 1
+
+
+def truncated_gumbel(alpha, truncation):
+    assert alpha.size(0) == truncation.size(0)
+    assert len(alpha.size()) == 2  # batch by items
+    assert len(truncation.size()) == 1  # batch
+
+    # By Chris J. Maddison and Danny Tarlow https://cmaddis.github.io/gumbel-machinery
+    gumbel = torch.distributions.gumbel.Gumbel(alpha, alpha.new_ones(alpha.size())).sample()
+    return -torch.log(torch.exp(-gumbel) + torch.exp(-truncation[:, None]))
+
+
+def max_gumbel(alpha, max):
+    assert alpha.size(0) == max.size(0)
+    assert len(alpha.size()) == len(max.size())
+    # https://arxiv.org/pdf/1903.06059.pdf appendix b
+
+    gumbel = torch.distributions.gumbel.Gumbel(alpha, alpha.new_ones(alpha.size())).sample()
+    Z = gumbel.max(dim=-1, keepdim=True).values
+
+    vi = max - gumbel + log1mexp(gumbel - Z)
+    ret = max - torch.clamp(vi, min=0) - log1pexp(-vi.abs())
+    return ret
+
+
+def log_importance_weight(kappa, phi):
+    return phi - np.log(gumbel_r.sf(kappa, loc=phi, scale=1))
