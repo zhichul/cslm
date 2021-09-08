787775f7f84d32f504919db84a018df3aa66aa96
Brian Lu <blu@Brians-MacBook-Pro-19.local>
2021-09-07 21:37:06 -0400

    Add tree annotated grammar

--- src/cslm/data/generation/grammars.py
+++ src/cslm/data/generation/grammars.py
@@ -72,3 +72,26 @@ def abstract_grammar_3(nouns_per_lang=10, shared_nouns=10, verbs=10, adjs=10):
     for i in range(adjs):
         G.add_rule(SyncRule(1.0, "ADJ", [[f"adj{i + 1}"], [f"adj{i + 1}-1"], [f"adj{i + 1}-2"]]))
     return G
+
+def abstract_grammar_2t(nouns=10, verbs=10, adjs=10):
+    """
+    Template grammar with noun-adj order difference between L1 and L2, plus having noun phrases.
+    """
+    G = SyncGrammar([
+        SyncRule(1.0, "ROOT", [["SNP:1", "VP:1"], ["SNP:1", "VP:1"],["SNP:1", "VP:1"]]),
+        SyncRule(1.0, "SNP", [["subj", "NP:1"], ["NP:1"], ["NP:1"]]),
+        SyncRule(1.0, "ONP", [["obj", "NP:1"], ["NP:1"], ["NP:1"]]),
+        SyncRule(1.0, "VP", [["verb", "VERB:1", "ONP:1"], ["VP-1", "VERB:1", "ONP:1"], ["VP-2","VERB:1", "ONP:1"]]),
+        SyncRule(1.0, "NP", [["NOUN:1", "mod", "ADJ:1"], ["NP-1", "ADJ:1", "NOUN:1"], ["NP-2", "NOUN:1", "ADJ:1"]]),
+        SyncRule(1.0, "NP", [["NOUN:1"], ["NP-1", "NOUN:1"], ["NP-2", "NOUN:1"]])
+    ], num_langs=3)
+    # noun
+    for i in range(nouns):
+        G.add_rule(SyncRule(1.0, "NOUN", [[f"noun{i + 1}"], ["N-1", f"noun{i + 1}-1"], ["N-2", f"noun{i + 1}-2"]]))
+    # verb
+    for i in range(verbs):
+        G.add_rule(SyncRule(1.0, "VERB", [[f"verb{i + 1}"], ["V-1", f"verb{i + 1}-1"], ["V-2", f"verb{i + 1}-2"]]))
+    # adjective
+    for i in range(adjs):
+        G.add_rule(SyncRule(1.0, "ADJ", [[f"adj{i + 1}"], ["A-1", f"adj{i + 1}-1"], ["A-2", f"adj{i + 1}-2"]]))
+    return G
